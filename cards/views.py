bfff85dfbc8c1b9cd1253127e9841a7633dcbb8a
mqashoo77 <mohammadqashoo77@gmail.com>
2022-12-23 16:03:58 +0200

    Update an Existing Card

--- cards/views.py
+++ cards/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
+    UpdateView,
 )
 from .models import Card
 
@@ -14,3 +15,7 @@ class CardCreateView(CreateView):
     model = Card
     fields = ["question", "answer", "box"]
     success_url = reverse_lazy("card-create")
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy("card-list")
+
