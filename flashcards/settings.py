4402b104071610e14fce783926bfdf0c69d3d4f5
Mohammad Qashoo <mohammadqashoo77@gmail.com>
2024-01-06 00:50:32 +0200

    update .env

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+0j44$+=x9oa^uoqfsjik)m1%1$oxufbt1jtqaqx(oyz6dv#ia"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
